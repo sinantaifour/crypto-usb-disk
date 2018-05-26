55ad5eb3eb410cc0cd85abeb4e8036465833a6bc
Sinan Taifour <sinan@taifour.com>
2018-05-27 01:21:29 +0200

    Nicer welcome message.

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ const { bold } = require('chalk');
 const { merge, progress, print, error, when, fill, proxy } = require('./util');
 const setup = require('./setup');
 
-print("");
-print(bold("Welcome to the Crypto USB Disk!"));
-print("Make sure you only run this on an offline computer.");
-print("");
+print(          "+-----------------------------------------------------+");
+print("|" + bold("          Welcome to the Crypto USB Disk!            ") + "|");
+print(          "| Make sure you only run this on an offline computer. |");
+print(          "+-----------------------------------------------------+");
 
 setup().then((seed) => {
   // TODO: Continue.
