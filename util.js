9e0576a8bf51d07cccdefd7a6aa7fd73deb87b38
Sinan Taifour <sinan@taifour.com>
2018-05-27 15:08:04 +0200

    Print a prompt in front of everything, to stay consistent with the inquirer look.

--- util.js
+++ util.js
@@ -1,9 +1,11 @@
 'use strict';
-const { yellow, red, dim, bold } = require('chalk');
+const { yellow, red, green, dim, bold } = require('chalk');
+
+const PROMPT = green(">") + " ";
 
 var merge = function(a, b) { return Object.assign({}, a, b); };
-var p = function (x, k) { process.stdout.write(Buffer.from(k ? x : yellow(x))); };
-var error = function(x) { p(red.underline.bold("Error:") + " " + red.bold(x), true); };
+var p = function (x, p, c) { process.stdout.write(Buffer.from((p ? "" : PROMPT) + (c ? x : yellow(x)))); };
+var error = function(x) { p(red.underline.bold("Error:") + " " + red.bold(x), false, true); };
 var print = function(x, y) {
   if (y) {
     p(x + ": " + bold(y) + "\n");
@@ -14,7 +16,7 @@ var print = function(x, y) {
 var progress = function(x, f) {
   p(x + ", this might take a while... ");
   var res = f();
-  print(dim("Done!"));
+  p(dim("Done!") + "\n", true);
   return res;
 };
 var proxy = function() {
